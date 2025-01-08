fe1eed6b8ed0cdb1f0db95d4134a1e6712c80496
elva <jia_shen@126.com>
2025-01-08 17:38:12 +0800

    initial commit

---
+++ components/Header/menuData.tsx
@@ -0,0 +1,87 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    newTab: false,
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "About us",
+    newTab: false,
+    path: "/#features",
+  },
+  {
+    id: 3,
+    title: "Boot Cap",
+    newTab: false,
+    submenu: [
+      {
+        id: 31,
+        title: "Blog Grid",
+        newTab: false,
+        path: "/blog",
+      },
+      {
+        id: 34,
+        title: "Sign In",
+        newTab: false,
+        path: "/auth/signin",
+      },
+      {
+        id: 35,
+        title: "Sign Up",
+        newTab: false,
+        path: "/auth/signup",
+      },
+      {
+        id: 35,
+        title: "Docs",
+        newTab: false,
+        path: "/docs",
+      },
+      {
+        id: 35.1,
+        title: "Support",
+        newTab: false,
+        path: "/support",
+      },
+      {
+        id: 36,
+        title: "404",
+        newTab: false,
+        path: "/error",
+      },
+    ],
+  },
+  {
+    id: 2.1,
+    title: "Pre-Registration",
+    newTab: false,
+    path: "/blog",
+  },
+  {
+    id: 2.3,
+    title: "Professional Advancement Series",
+    newTab: false,
+    path: "/docs",
+  },
+
+
+  {
+    id: 4,
+    title: "Pro Picks",
+    newTab: false,
+    path: "/support",
+  },
+  {
+    id: 5,
+    title: "Contact Us",
+    newTab: false,
+    path: "/support",
+  },
+];
+
+export default menuData;
