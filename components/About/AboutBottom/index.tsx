f834ba0a758439c8c3ff02b5c0bd10d0d29ae3cc
elva <jia_shen@126.com>
2025-01-12 15:57:58 +0800

    feat: updated why learn

--- components/About/AboutBottom/index.tsx
+++ components/About/AboutBottom/index.tsx
@@ -4,19 +4,19 @@ import Image from "next/image";
 const AboutBottom = () => {
   return (
     <div className='grid grid-cols-2 gap-2 bg-[#fefefe]'>
-      <div className='flex flex-col pr-10 pl-30'>
+      <div className='flex flex-col pr-20 pl-20'>
         <div className="relative text43xl font-bold text-[#3b065f] xl:text-hero pb-10">
           Are You Career Ready? Unlock Your <span className='bg-gradient-to-r from-purple-600 via-red-500 to-indigo-400 text-transparent bg-clip-text'>Potential </span>
           with LearnNex
         </div>
-        <div>Your journey towards a fulfilling career starts here. LearnNex equips you with the skills, knowledge, and confidence you need to seize the opportunities that come your way. Join us and be prepared to embark on a rewarding tech journey.</div>
+        <div className='text-sm text-[#627693]'>Your journey towards a fulfilling career starts here. LearnNex equips you with the skills, knowledge, and confidence you need to seize the opportunities that come your way. Join us and be prepared to embark on a rewarding tech journey.</div>
       </div>
       <Image
-        className="m-auto"
+        className="m-auto aspect-[489/433]"
         src='/images/about/about-bottom.png'
         alt="Hero"
-        width={438}
-        height={438}
+        width={498}
+        height={433}
       />
     </div>
   )
--- components/WhyLearn/index.tsx
+++ components/WhyLearn/index.tsx
@@ -8,7 +8,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
-
+import Image from "next/image";
 
 const WhyLearn = () => {
   const [activeFaq, setActiveFaq] = useState(1);
@@ -18,57 +18,59 @@ const WhyLearn = () => {
   };
 
   return (
-    <>
-      {/* <!-- ===== FAQ Start ===== --> */}
-      <section className="overflow-hidden pb-20 lg:pb-15 xl:pb-20 bg-white">
-        <div className="relative mx-auto max-w-c-1235 px-4 md:px-8 xl:px-0">
-          <div className="absolute -bottom-16 -z-1 h-full w-full">
+    <div className='grid grid-cols-2 gap-2 bg-[#fefefe] mb-20 mt-20'>
+      <div className='flex flex-col pl-10'>
+        <motion.div
+          variants={{
+            hidden: {
+              opacity: 0,
+              x: -20,
+            },
+
+            visible: {
+              opacity: 1,
+              x: 0,
+            },
+          }}
+          initial="hidden"
+          whileInView="visible"
+          transition={{ duration: 1, delay: 0.1 }}
+          viewport={{ once: true }}
+          className="animate_left text-center"
+        >
+          <div className="relative text43xl font-bold text-[#3b065f] xl:text-hero">
+            Why LearnNex: Redefining <span className='bg-gradient-to-r from-purple-600 via-red-500 to-indigo-400 text-transparent bg-clip-text'>Excellence</span>
           </div>
-          <div className="flex flex-wrap gap-8 md:flex-nowrap md:items-center xl:gap-12.5 flex-col">
-            <motion.div
-              variants={{
-                hidden: {
-                  opacity: 0,
-                  x: -20,
-                },
+        </motion.div>
 
-                visible: {
-                  opacity: 1,
-                  x: 0,
-                },
-              }}
-              initial="hidden"
-              whileInView="visible"
-              transition={{ duration: 1, delay: 0.1 }}
-              viewport={{ once: true }}
-              className="animate_left md:w-2/5 lg:w-1/2 mt-11"
-            >
-              <div className="relative text43xl font-bold text-[#3b065f] xl:text-hero">
-                Why LearnNex: Redefining <span className='bg-gradient-to-r from-purple-600 via-red-500 to-indigo-400 text-transparent bg-clip-text'>Excellence</span>
-              </div>
-            </motion.div>
-
-            <Accordion type="single" collapsible className="w-full pr-50 pl-50">
-              {whyData.map((faq, key) => (
-                <AccordionItem value={`item-${faq.id}`} key={faq.id}
-                  className='
+        <Accordion type="single" collapsible className="w-full">
+          {whyData.map((faq, key) => (
+            <AccordionItem value={`item-${faq.id}`} key={faq.id}
+              className='
                     data-[state=open]:bg-[#f8f7fc] 
                     py-8
                     px-10
                     rounded-3xl
                     '>
-                  <AccordionTrigger>{faq.quest}</AccordionTrigger>
-                  <AccordionContent>
-                    {faq.ans}
-                  </AccordionContent>
-                </AccordionItem>
-              ))}
-            </Accordion>
-          </div>
-        </div>
-      </section>
-      {/* <!-- ===== FAQ End ===== --> */}
-    </>
+              <AccordionTrigger>{faq.quest}</AccordionTrigger>
+              <AccordionContent>
+                {faq.ans}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+      <div>
+        <Image
+          className="m-auto aspect-[555/548]"
+          src='/images/about/why-learn.png'
+          alt="Hero"
+          width={555}
+          height={548}
+        />
+      </div>
+
+    </div>
   );
 };
 
